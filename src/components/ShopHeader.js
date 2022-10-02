4fe0a671b8a7d1cc99659b729ceb78a8aebae6c2
leeseoeun <seoeun.shuen@gmail.com>
2022-10-02 20:24:32 +0900

    1002 migration sample

---
+++ src/components/ShopHeader.js
@@ -0,0 +1,17 @@
+function ShopHeader() {
+    return(
+        <header>
+            <img src="https://dummyimage.com/200x100/000/fff&text=Logo" alt="" />
+            <nav>
+                <ul>
+                <li><a href="">Videos to rent</a></li>
+                <li><a href="">Videos to buy</a></li>
+                <li><a href="">Used DVDs</a></li>
+                <li><a href="">Offers</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default ShopHeader;
