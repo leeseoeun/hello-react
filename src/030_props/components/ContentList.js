3f828a6d4366d7a625a44ef509bea4d015182134
leeseoeun <seoeun.shuen@gmail.com>
2022-10-03 01:10:32 +0900

    1003 state hook

---
+++ src/030_props/components/ContentList.js
@@ -0,0 +1,39 @@
+function ContentList() {
+    return (
+        <section class="product-cards">
+            <div class="product-card">
+                <img src="https://dummyimage.com/300x300/7EC0EE/000&text=Product+Image+1" alt="" />
+                <h2><a href="">Video title 1</a></h2>
+                <p class="original-price">RRP: $18.99</p>
+                <p class="current-price">Price you pay <span>$9.99</span></p>
+                <p class="saving">Your saving $9</p>
+            </div>
+
+            <div class="product-card">
+                <img src="https://dummyimage.com/300x300/7EC0EE/000&text=Product+Image+2" alt="" />
+                <h2><a href="">Video title 2</a></h2>
+                <p class="original-price">RRP: $18.99</p>
+                <p class="current-price">Price you pay <span>$9.99</span></p>
+                <p class="saving">Your saving $9</p>
+            </div>
+
+            <div class="product-card">
+                <img src="https://dummyimage.com/300x300/7EC0EE/000&text=Product+Image+3" alt="" />
+                <h2><a href="">Video title 3</a></h2>
+                <p class="original-price">RRP: $18.99</p>
+                <p class="current-price">Price you pay <span>$9.99</span></p>
+                <p class="saving">Your saving $9</p>
+            </div>
+
+            <div class="product-card">
+                <img src="https://dummyimage.com/300x300/7EC0EE/000&text=Product+Image+4" alt="" />
+                <h2><a href="">Video title 4</a></h2>
+                <p class="original-price">RRP: $18.99</p>
+                <p class="current-price">Price you pay <span>$9.99</span></p>
+                <p class="saving">Your saving $9</p>
+            </div>
+        </section>
+    );
+}
+
+export default ContentList;
